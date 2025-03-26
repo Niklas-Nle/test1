428335153b70f7d67ac04efa47f5f6388a87fa17
Niklas <niessleinniklas@bs-intra.de>
2025-03-26 11:01:22 +0100

    hi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Aktiviert den statischen Export
 };
 
 export default nextConfig;
